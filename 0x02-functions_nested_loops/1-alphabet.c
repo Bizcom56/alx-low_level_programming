70e7e593de3aa874d79c22a381347737648a1ce0
Bizcom56 <dornorjoshua56@gmail.com>
2023-02-21 19:09:19 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,16 +3,18 @@
 /**
  * print_alphabet - Prints the alphabet in lowercase.
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
 
 void print_alphabet(void)
-{
-	char c;
 
-	for (c = 'a'; c <= z; c++)
-	{
-	_putchar(c);
-	}
-	_putchar('\n');
+{
+
+    	char c;
+
+    	for (c = 'a'; c <= 'z'; c++)
+    	{
+    	_putchar(c);
+    	}
+    	_putchar('\n');
 }
