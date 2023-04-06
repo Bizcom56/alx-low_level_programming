9e5f7336afc8cb196896391af772a924c6852b41
Bizcom56 <dornorjoshua56@gmail.com>
2023-04-05 17:53:02 -0700

    More singly linked lists

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,26 +1,66 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list and sets the head to NULL
- * @head: pointer to head of a list
+ * free_listp2 - frees a linked list
+ * @head: head of a list.
  *
- * Return: no return
+ * Return: no return.
  */
-void free_listint2(listint_t **head)
+void free_listp2(listp_t **head)
 {
-	listint_t *current;
-
-	if (head == NULL)
-	{
-		return;
-	}
+	listp_t *temp;
 
 	while (*head != NULL)
 	{
-		current = *head;
-		*head = current->next;
-		free(current)
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+}
+
+/**
+ * free_listint_safe - frees a linked list.
+ * @h: head of a list.
+ *
+ * Return: size of the list that was freed.
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t num_nodes = 0;
+	listp_t *hptr = NULL, *new = NULL;
+	listint_t *current = NULL;
+
+	while (*h != NULL)
+	{
+		/* Create a new node and add it to the head of the tracking list */
+		new = malloc(sizeof(listp_t));
+		if (new == NULL)
+			exit(98);
+
+		new->p = (void *)*h;
+		new->next = hptr;
+		hptr = new;
+
+		/* Check for a loop by traversing the tracking list */
+		for (listp_t *tmp = hptr; tmp != NULL; tmp = tmp->next)
+		{
+			if (*h == tmp->p)
+			{
+				/* Loop detected, free the tracking list and exit */
+				free_listp2(&hptr);
+				*h = NULL;
+				return (num_nodes);
+			}
+		}
+
+		/* No loop detected, free the currentent node and move to the next */
+		current = *h;
+		*h = (*h)->next;
+		free(current);
+		num_nodes++;
 	}
 
-	*head = (NULL);
+	/* All nodes have been freed, free the tracking list and exit */
+	free_listp2(&hptr);
+	return (num_nodes);
 }
