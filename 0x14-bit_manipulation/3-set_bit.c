5dad85f4213e7daa59f2dbd40d3923c46b6eb8fe
“Bizcom56” <dornorjoshua56@gmail.com>
2023-09-29 01:17:08 -0700

    bits

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * set_bit - Sets the value of a bit to 1 at a specified index.
+ *
+ * @n: Pointer to an unsigned long int.
+ * @index: The index of the bit to set.
+ *
+ * Return: 1 if successful, -1 if not.
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+	*n = (*n | m);
+
+	return (1);
+}
---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * clear_bit - Sets the value of a bit to 0 at a specified index.
+ *
+ * @n: Pointer to an unsigned long int.
+ * @index: The index of the bit to clear.
+ *
+ * Return: 1 if successful, -1 if not.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int m;
+
+	if (index > 63)
+		return (-1);
+
+	m = 1 << index;
+
+	if (*n & m)
+		*n ^= m;
+
+	return (1);
+}
